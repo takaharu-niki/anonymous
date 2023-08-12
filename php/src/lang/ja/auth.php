62a331e186563ad218d65d5b770473ec7aa6a645
仁木崇陽 <takaharu@MacBook-Pro.local>
2023-08-12 11:36:01 +0900

    change messages from en to ja

--- php/src/lang/ja/auth.php
+++ php/src/lang/ja/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
     'password' => 'パスワードが正しくありません。',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'ログイン情報が登録されていません。',
+    'throttle' => 'ログインに続けて失敗しています。:seconds秒後に再度お試しください。',
 
 ];
--- php/src/lang/ja/passwords.php
+++ php/src/lang/ja/passwords.php
@@ -13,10 +13,9 @@ return [
     |
     */
 
-    'reset' => 'Your password has been reset.',
-    'sent' => 'We have emailed your password reset link.',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
-
+    'reset' => 'パスワードをリセットしました。',
+    'sent' => 'パスワードリセットメールを送信しました。',
+    'throttled' => 'しばらく再試行はお待ちください。',
+    'token' => 'このパスワードリセットトークンは無効です。',
+    'user' => "メールアドレスに一致するユーザーは存在していません。",
 ];
